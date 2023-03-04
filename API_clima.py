dd634aa164e1d38188be6e78f9594df8e4487c5f
Lucas Gabriel <114539692+LucasGdBS@users.noreply.github.com>
2023-03-04 16:22:37 -0300

    Uma busca na API de clima

---
+++ API_clima.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+url = 'https://weather.contrateumdev.com.br/api/weather/city/?city=Olinda,Pernambuco'
+
+response = requests.get(url=url)
+
+clima = response.json()
+
+#print(clima)
+
+
+print('Temp: ', clima['main']['temp'])
+print('Sensação termica: ', clima['main']['feels_like'])
+print('temp minima: ', clima['main']['temp_min'])
+print('temp max: ', clima['main']['temp_max'])
+print('Umidade: ', clima['main']['humidity'], '%')
+print('Tempo: ', clima['weather'][0]['description'])
+
+
